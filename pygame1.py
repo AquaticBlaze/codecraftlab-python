ddceb9276410eac909a9d3d55a47e67cb4d3df2c
AquaticBlaze <mateokimball@gmail.com>
2016-09-21 17:57:35 -0400

    Add files via upload

--- pygame1.py
+++ pygame1.py
@@ -1,8 +1,61 @@
 import pygame
+import random
 from pygame.locals import *
 
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Player, self).__init__()
+        self.surf = pygame.Surface((75, 75))
+        self.surf.fill((255, 255, 255))
+        self.rect = self.surf.get_rect()
+
+    def update(self, pressed_keys):
+        if pressed_keys[K_UP]:
+            self.rect.move_ip(0, -2)
+        if pressed_keys[K_DOWN]:
+            self.rect.move_ip(0, 2)
+        if pressed_keys[K_LEFT]:
+            self.rect.move_ip(-2, 0)
+        if pressed_keys[K_RIGHT]:
+            self.rect.move_ip(2, 0)
+            
+        if self.rect.left < 0:
+            self.rect.left = 0
+        elif self.rect.right > 1280:
+            self.rect.right = 1280
+        if self.rect.top <= 0:
+            self.rect.top = 0
+        elif self.rect.bottom >= 1024:
+            self.rect.bottom = 1024
+
+class Opponent(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Opponent, self).__init__()
+        self.surf = pygame.Surface((20, 10))
+        self.surf.fill((255, 255, 255))
+        self.rect = self.surf.get_rect(center=(820, random.randint(0, 600)))
+        self.speed = random.randint(0, 2)
+
+    def update(self):
+        self.rect.move_ip(-self.speed, 0)
+        if self.rect.right < 0:
+            self.kill()
+        
+        
 pygame.init()
 screen = pygame.display.set_mode((1280, 1024))
+player = Player()
+background = pygame.Surface(screen.get_size())
+background.fill((0, 0, 0))
+players = pygame.sprite.Group()
+opponents = pygame.sprite.Group()
+all_sprites = pygame.sprite.Group()
+all_sprites.add(player)
+ADDOPPONENT = pygame.USEREVENT + 1
+pygame.time.set_timer(ADDOPPONENT, 250)
+#surf = pygame.Surface((75, 75))
+#surf.fill((255, 255, 255))
+#rect = surf.get_rect()
 
 running = True
 while running:
@@ -12,8 +65,19 @@ while running:
                 running = False
         elif event.type == QUIT:
             running = False
+        elif(event.type == ADDOPPONENT):
+            new_opponent = Opponent()
+            opponents.add(new_opponent)
+            all_sprites.add(new_opponent)
 
-
+    screen.blit(background, (0, 0))
+    pressed_keys = pygame.key.get_pressed()
+    player.update(pressed_keys)
+    opponents.update()
+    
+    for entity in all_sprites:
+        screen.blit(entity.surf, entity.rect)
+    pygame.display.flip()
 
 
 
