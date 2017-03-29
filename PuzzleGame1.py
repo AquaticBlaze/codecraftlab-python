3a967084d7cd4b410b0e02ddc095342bfa15dde8
AquaticBlaze <mateokimball@gmail.com>
2017-03-29 18:09:03 -0400

    Add files via upload

---
+++ PuzzleGame1.py
@@ -0,0 +1,27 @@
+import pygame
+from pygame import *
+pygame.init()
+
+screen = pygame.display.set_mode((750, 540))
+background.fill((0, 0, 0))
+
+run = True
+while run:
+
+    for event in pygame.event.get():
+        
+        if event.type == QUIT:
+            run = False
+        if event.type == KEYDOWN and event.key == K_ESCAPE:
+            run = False
+        
+pygame.quit()            
+
+    
+
+
+
+
+
+
+
