1d188c56defd062973f96a491395c5af19ffcedf
AquaticBlaze <mateokimball@gmail.com>
2017-04-05 18:01:46 -0400

    Add files via upload

--- PuzzleGame1.py
+++ PuzzleGame1.py
@@ -2,20 +2,61 @@ import pygame
 from pygame import *
 pygame.init()
 
+class P1(pygame.sprite.Sprite):
+    def __init__(self):
+        super(P1, self).__init__()
+        self.image = pygame.image.load('P1.png').convert()
+        self.image.set_colorkey((255, 255, 255), RLEACCEL)
+        self.rect = self.image.get_rect(center=(375, 270))
+
+class P2(pygame.sprite.Sprite):
+    def __init__(self):
+        super(P1, self).__init__()
+        self.image = pygame.image.load('P2.png').convert()
+        self.image.set_colorkey((255, 255, 255), RLEACCEL)
+        self.rect = self.image.get_rect(center=(375, 270))
+
+puzzle = 1
 screen = pygame.display.set_mode((750, 540))
 background = pygame.Surface(screen.get_size())
-background.fill((0, 0, 0))
+background.fill((60, 57, 21))        
+p1 = P1()
+p2 = P2()
+all_sprites = pygame.sprite.Group()
+all_sprites.add(p1)
 
 run = True
 while run:
+    screen.blit(background, (0, 0))
+    for entity in all_sprites:
+        screen.blit(entity.image, entity.rect)
+    pygame.display.flip()
 
     for event in pygame.event.get():
         
         if event.type == QUIT:
             run = False
-        if event.type == KEYDOWN and event.key == K_ESCAPE:
+        elif event.type == KEYDOWN and event.key == K_ESCAPE:
             run = False
+
+    if puzzle == 1:
+            print('What is the answer?')
+            answer = raw_input()
+            if answer == "61013":
+                puzzle = 2
+                p1.kill()
+                all_sprites.add(p2)
+    if puzzle == 2:
+        print('What is the answer?')
+            answer = raw_input()
+            if answer == "mops":
+                puzzle = 3
+                p2.kill()
+                #all_sprites.add(p3)
         
+                
+
+
 pygame.quit()            
 
     
